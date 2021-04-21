3ff4a77bdd8cfe58e6d5ca74c0de6c6cab2f20c7
nonnavolodina <volodina.nonna@gmail.com>
2021-04-21 17:01:54 -0400

    Started to add basic html elements and styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
-	<header></header>
-	<main></main>
-	<footer></footer>
+	<header class="header">
+        <h1 class="header__title">Vue Habit Tracker</h1>
+    </header>
+	<main class="app">
+        <p class="app__prompt">What habit would you like to track?</p>
+        <div class="form">
+            <input class="form__input" type="text">
+            <button class="form__submit">Submit</button>
+        </div>
+	</main>
+	<footer>
+        <p>footer</p>
+    </footer>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+	name: 'App',
 }
 </script>
 
 <style lang="scss">
-@import "./styles/main.scss";
+	@import "./styles/styles.scss";
 </style>
