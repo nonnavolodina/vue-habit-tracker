a1fab7745d419343c6c3e3977626dc1dc9834eb0
nonnavolodina <volodina.nonna@gmail.com>
2021-04-22 19:06:10 -0400

    Updated markup location for header and footer, and added first prompt

--- src/components/HabitTrack.vue
+++ src/components/HabitTrack.vue
@@ -1,7 +1,30 @@
 <template>
     <div class="habit-track">
-        <input class="habit-track__input" type="text">
-        <button class="habit-track__submit">Track</button>
+        <input v-model="habit" @input="setHabit" class="habit-track__input" type="text">
+        <button @click="showHabitTrackingPrompts" class="habit-track__submit">Submit</button>
+        <div v-if="isHidden === true" class="habit-tracking-prompts">
+            <div class="timeline">
+                <p class="timeline__description">How often do you want to track this habit?</p>
+                <div class="timeline__form">
+                    <div class="timeline__radio">
+                        <input id="daily" class="timeline__input" type="radio" name="timeline" value="daily" v-model="checked">
+                        <label for="daily" class="timeline__label">Daily</label>
+                    </div>
+                    <div class="timeline__radio">
+                        <input id="weekly" class="timeline__input" type="radio" name="timeline" value="weekly" v-model="checked">
+                        <label for="weekly" class="timeline__label">Weekly</label>
+                    </div>
+                    <div class="timeline__radio">
+                        <input id="monthly" class="timeline__input" type="radio" name="timeline" value="monthly" v-model="checked">
+                        <label for="monthly" class="timeline__label">Monthly</label>
+                    </div>
+                    <div class="timeline__radio">
+                        <input id="yearly" class="timeline__input" type="radio" name="timeline" value="yearly" v-model="checked">
+                        <label for="yearly" class="timeline__label">Yearly</label>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,8 +32,19 @@
 export default {
     data() {
         return {
-
+            habit: '',
+            isHidden: false,
+            checked: ''
         };
     },
+    methods: {
+        setHabit(event) {
+            this.habit = event.target.value;
+        },
+        showHabitTrackingPrompts() {
+            this.isHidden = !this.isHidden;
+        },
+    },
+
 };
 </script>
