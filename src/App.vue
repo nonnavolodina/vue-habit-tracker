ad25afdcd7d60f47820a6b5b00c53e16fb10e7e3
nonnavolodina <volodina.nonna@gmail.com>
2021-04-23 16:39:36 -0400

    Added additional section to habit tracking prompts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
 	<main class="app">
-        <p class="app__description">Vue Habit Tracker is a simple app built using the Vue CLI. Enter a habit that you would like to work on (e.g., drink more water, walk 30 minutes a day, go to bed by 10 o'clock PM), and follow the prompts to let the Vue Habit Tracker help you stay on top of your goals and lead a more productive lifestyle.</p>
+        <div class="app__description">
+            <p class="app__description-copy">
+                Vue Habit Tracker is a simple app built using the Vue CLI. Enter a habit that you would like to work on (e.g., drink 8 glasses of water, go for a 30 minute walk, go to bed by 9 o'clock PM), and follow the prompts to let the Vue Habit Tracker help you stay on top of your goals and lead a more productive lifestyle.
+            </p>
+            <p class="app__description-copy">Expert tip: Make sure your habit goal is specific. Instead of 'drink more water', your habit should aim for a certain goal. So instead of 'drink more water', you should try 'drink 8 glasses of water a day'.</p>
+        </div>
         <p class="app__prompt">What habit would you like to track?</p>
         <habit-track></habit-track>
 	</main>
--- src/components/HabitTrack.vue
+++ src/components/HabitTrack.vue
@@ -1,28 +1,118 @@
 <template>
     <div class="habit-track">
         <div class="habit-track__form">
-            <input v-model="habit" @input="setHabit" class="habit-track__input" type="text">
-            <button @click="showHabitTrackingPrompts" class="habit-track__submit">Submit</button>
+            <input v-model="habit" @input="setHabit" @keyup.enter="showTimelinePrompt" placeholder="Examples: walk 30 minutes a day, drink more water" class="habit-track__input" type="text">
+            <button @click="showTimelinePrompt" class="habit-track__submit">Submit</button>
         </div>
-        <div v-if="isHidden === true" class="habit-tracking-prompts">
-            <div class="timeline">
-                <p class="timeline__description">How often do you want to track this habit?</p>
+        <div class="habit-tracking-prompts">
+            <div v-if="timelineHidden === true" class="timeline">
+                <p class="timeline__description">How often do you want to {{ habit }}?</p>
                 <div class="timeline__form">
                     <div class="timeline__radio">
-                        <input id="daily" class="timeline__input" type="radio" name="timeline" value="daily" v-model="checked">
+                        <input id="daily" class="timeline__input" type="radio" name="timeline" value="day" v-model="timeline" @click="showGoalPrompt">
                         <label for="daily" class="timeline__label">Daily</label>
                     </div>
                     <div class="timeline__radio">
-                        <input id="weekly" class="timeline__input" type="radio" name="timeline" value="weekly" v-model="checked">
+                        <input id="weekly" class="timeline__input" type="radio" name="timeline" value="week" v-model="timeline" @click="showGoalPrompt">
                         <label for="weekly" class="timeline__label">Weekly</label>
                     </div>
-                    <div class="timeline__radio">
-                        <input id="monthly" class="timeline__input" type="radio" name="timeline" value="monthly" v-model="checked">
-                        <label for="monthly" class="timeline__label">Monthly</label>
+                </div>
+            </div>
+            <div v-if="timeline === 'week'" class="weekly-schedule">
+                <p class="weekly-schedule__description">Which days of the week do you want to {{ habit }}?</p>
+                <div class="weekly-schedule__form">
+                    <div class="weekly-schedule__checkbox">
+                        <input id="sunday" class="weekly__schedule" type="checkbox" name="week" value="sunday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="sunday" class="weekly-schedule__label">Sunday</label>
                     </div>
-                    <div class="timeline__radio">
-                        <input id="yearly" class="timeline__input" type="radio" name="timeline" value="yearly" v-model="checked">
-                        <label for="yearly" class="timeline__label">Yearly</label>
+                    <div class="weekly-schedule__checkbox">
+                        <input id="monday" class="weekly__schedule" type="checkbox" name="week" value="monday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="monday" class="weekly-schedule__label">Monday</label>
+                    </div>
+                    <div class="weekly-schedule__checkbox">
+                        <input id="tuesday" class="weekly__schedule" type="checkbox" name="week" value="tuesday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="tuesday" class="weekly-schedule__label">Tuesday</label>
+                    </div>
+                    <div class="weekly-schedule__checkbox">
+                        <input id="wednesday" class="weekly__schedule" type="checkbox" name="week" value="wednesday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="wednesday" class="weekly-schedule__label">Wednesday</label>
+                    </div>
+                    <div class="weekly-schedule__checkbox">
+                        <input id="thursday" class="weekly__schedule" type="checkbox" name="week" value="thursday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="thursday" class="weekly-schedule__label">Thursday</label>
+                    </div>
+                    <div class="weekly-schedule__checkbox">
+                        <input id="friday" class="weekly__schedule" type="checkbox" name="week" value="friday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="friday" class="weekly-schedule__label">Friday</label>
+                    </div>
+                    <div class="weekly-schedule__checkbox">
+                        <input id="saturday" class="weekly__schedule" type="checkbox" name="week" value="saturday" v-model="daysOfTheWeek" @click="showColoursPrompt">
+                        <label for="saturday" class="weekly-schedule__label">Saturday</label>
+                    </div>
+                </div>
+            </div>
+            <div v-if="coloursHidden === true || timeline === 'day'" class="colour">
+                <p class="colour__description">Pick a colour for your {{ habit }} goal:</p>
+                <div class="colour__form">
+                    <div class="colour__radio">
+                        <input id="red" class="colur__input colour__input--red;" type="radio" name="colur" value="red" v-model="colur">
+                        <label for="red">
+                            <span class="colour__label red"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="green" class="colour__input" type="radio" name="colour" value="green" v-model="colour">
+                        <label for="green">
+                            <span class="colour__label green"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="yellow" class="colour__input" type="radio" name="colour" value="yellow" v-model="colour">
+                        <label for="yellow">
+                            <span class="colour__label yellow"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="olive" class="colour__input" type="radio" name="colour" value="olive" v-model="colour">
+                        <label for="olive">
+                            <span class="colour__label olive"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="orange" class="colour__input" type="radio" name="colour" value="orange" v-model="colour">
+                        <label for="orange">
+                            <span class="colour__label orange"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="teal" class="colour__input" type="radio" name="colour" value="teal" v-model="colour">
+                        <label for="teal">
+                            <span class="colour__label teal"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="blue" class="colour__input" type="radio" name="colour" value="blue" v-model="colour">
+                        <label for="blue">
+                            <span class="colour__label blue"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="violet" class="colour__input" type="radio" name="colour" value="violet" v-model="colour">
+                        <label for="violet">
+                            <span class="colour__label violet"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="purple" class="colour__input" type="radio" name="colour" value="purple" v-model="colour">
+                        <label for="purple">
+                            <span class="colour__label purple"></span>
+                        </label>
+                    </div>
+                    <div class="colour__radio">
+                        <input id="pink" class="colour__input" type="radio" name="colour" value="pink" v-model="colour">
+                        <label for="pink">
+                            <span class="colour__label pink"></span>
+                        </label>
                     </div>
                 </div>
             </div>
@@ -35,17 +125,27 @@ export default {
     data() {
         return {
             habit: '',
-            isHidden: false,
-            checked: ''
+            timelineHidden: false,
+            timeline: '',
+            daysOfTheWeek: [],
+            weeklyScheduleHidden: false,
+            colour: '',
+            coloursHidden: false
         };
     },
     methods: {
         setHabit(event) {
             this.habit = event.target.value;
         },
-        showHabitTrackingPrompts() {
-            this.isHidden = !this.isHidden;
+        showTimelinePrompt() {
+            this.timelineHidden = !this.timelineHidden;
         },
+        showWeeklySchedulePrompt() {
+            this.weeklyScheduleHidden = !this.weeklyScheduleHidden;
+        },
+        showColoursPrompt() {
+            this.coloursHidden = !this.coloursHidden;
+        }
     },
 
 };
